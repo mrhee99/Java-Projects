fa6b6e9414febc511054bbe9fe1e9e34c164a933
Michelle Rhee <mrhee99@gmail.com>
2022-06-29 12:41:14 -0700

    hello number

---
+++ hello_number.java
@@ -0,0 +1,13 @@
+public class hello_number {
+    public static void  main(String[] args) {
+        int x = 0;
+        while (x<10)
+            {System.out.println (x);
+            x= x+1;
+            }
+    
+    }
+
+ // the result prints out 0 to 9 in seperate lines
+
+}
