bbb654e166b05b36eee1aae94510e6023106de1d
Michelle Rhee <mrhee99@gmail.com>
2022-06-29 14:01:19 -0700

    number_horse

---
+++ number_horse.java
@@ -0,0 +1,11 @@
+public class number_horse {
+    public static void  main(String[] args) {
+        int x = 0;
+        String h = x + "horse";   
+        while (x < 10){
+            x = x+1;
+            System.out.println (h);
+        }
+            
+    }
+} 
