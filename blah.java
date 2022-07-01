4e26a91b58776be1bdfa8aa5339c9e55c4afdf96
Michelle Rhee <mrhee99@gmail.com>
2022-07-01 14:00:03 -0700

    triangle function

---
+++ blah.java
@@ -0,0 +1,10 @@
+public class blah {
+    /** This program will print out number of bottles until it reaches 1 bottle.  */
+    public static void  main(String[] args) 
+    {int bottles = 99;
+    while (bottles > 0) {
+        System.out.println(bottles + " bottles of beer on the wall.");
+        // after every iteration, the number of bottle will decrease by 1
+        bottles = bottles - 1;}
+    }
+}
--- triangle.java
+++ triangle.java
@@ -3,11 +3,11 @@ public class triangle {
         int rows = 5;
         // outer loop for the number of rows
         // start with 1 row, as long as i is less than or equal to the row number, add 1 more row each time until it gets to 5
-        for (int i = 1; i <= rows; ++i) {
+        for (int i = 1; i <= rows; i++) {
 
         // inner loop for the number of columns 
         // start with 1 column, as long as j is less than or equal to the number of rows, add 1 more row each time until it becomes greater than i
-            for (int j = 1; j <= i; ++j) {
+            for (int j = 1; j <= i; j++) {
              System.out.print("* ");
         }
         // creates a new line after each iteration
